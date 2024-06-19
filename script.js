db0191df186b05bd0c5a069cbe147d1e85fca52f
fleeshka <uliana220604@mail.ru>
2024-06-19 17:22:50 +0300

    add script file

---
+++ script.js
@@ -0,0 +1,36 @@
+const email = 'u.chaikouskaya@innopolis.university';
+const searchParams = new URLSearchParams();
+searchParams.append('email', email);
+const apiUrl = `https://fwd.innopolis.university/api/hw2?${searchParams.toString()}`;
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(comicId => {
+        const getComicSearchParams = new URLSearchParams();
+        getComicSearchParams.append('id', comicId);
+        return fetch(`https://fwd.innopolis.university/api/comic?${getComicSearchParams.toString()}`);
+    })
+    .then(response => response.json())
+    .then(comic => {
+        displayComic(comic);
+    })
+   
+
+function displayComic(comic) {
+    const container = document.getElementById('comic-container');
+
+    const title = document.createElement('h2');
+    title.textContent = comic.title;
+
+    const img = document.createElement('img');
+    img.src = comic.img;
+    img.alt = comic.alt;
+
+    const date = document.createElement('p');
+    const publishDate = new Date(comic.year, comic.month - 1, comic.day);
+    date.textContent = `Published on: ${publishDate.toLocaleDateString()}`;
+
+    container.appendChild(title);
+    container.appendChild(img);
+    container.appendChild(date);
+}
