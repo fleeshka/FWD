a2db5013416a1d03155a44865c258f698fbcd652
Ulyana <93733741+fleeshka@users.noreply.github.com>
2024-07-03 19:34:51 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,4 @@
 const path = require('path');
-<<<<<<< Updated upstream
-const TerserPlugin = require('terser-webpack-plugin');
-
-module.exports = {
-    entry: './src/index.ts', // Основной файл TypeScript
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'], // Разрешаем расширения .ts и .js
-    },
-    output: {
-        filename: 'bundle.min.js', // Выходной файл с минификацией
-        path: path.resolve(__dirname, 'dist'), // Путь до папки dist
-    },
-    mode: 'production', // Режим продакшн для оптимизации
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    format: {
-                        comments: false, // Удаление комментариев при минификации
-                    },
-                },
-                extractComments: false,
-            }),
-        ],
-    },
-=======
 
 module.exports = {
   entry: './src/index.tsx',
@@ -73,5 +37,4 @@ module.exports = {
     port: 3001,
   },
   mode: 'development',
->>>>>>> Stashed changes
 };
